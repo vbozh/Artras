2e88d142891a9b49d1a263fbf7bd5e92d16138ee
ñÑùµüöäÜÖÄ <viktoria.bozhko@jetbrains.com>
2021-04-08 14:32:41 +0300

    ñÑùµüöäÜÖÄ

---
+++ src/com/company/MainTest.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
